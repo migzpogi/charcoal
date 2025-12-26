6001b964dd5ec02c6c25298525a813e6a559e5f3
migzpogi <migz.zgim@gmail.com>
2025-12-27 00:24:42 +0800

    Solves day 2

--- aoc2025/02/day2.py
+++ aoc2025/02/day2.py
@@ -31,15 +31,54 @@ def is_pid_invalid(pid):
         return False
 
     midpoint = int(len(pid)/2)
-
     if pid[0:midpoint] == pid[midpoint:]:
         return True
     else:
         return False
 
+def is_pid_invalid_v2(pid):
+    """
+    A product ID is invalid if it is made only of some sequence of digits repeated at least twice.
+    12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
 
+    :param pid:
+    :type pid: int
+
+    :returns: True if product ID is invalid, otherwise False
+    :rtype: bool
+    """
+    pid = str(pid)
+
+    # Hardcoded rule, these product ids are not invalid
+    if int(pid) <= 10:
+        return False
+
+    # If the number of digits are odd, then it will only be invalid if all digits are the same
+    if len(pid)%2 != 0:
+        if len(set(pid)) == 1:
+            return True
+
+    # Split the product id into two, if both halves are equal, then they are invalid
+    midpoint = int(len(pid)/2)
+    if pid[0:midpoint] == pid[midpoint:]:
+        return True
+    
+    # Given a product id 123412341234:
+    #   split(12) -> ['', '34', '34', '34']
+    #       unique_elemtents = 2, continue and increment split value
+    #   split(123) -> ['', '4', '4', '4']
+    #       unique_elemtents = 2, continue and increment split value
+    #   split(1234) -> ['', '', '', '']
+    #       unique_elemtents = 1, stop, product id is invalid
+    for i in range(0, midpoint):
+        possible_patterns = set(pid.split(pid[0:i+1]))
+        if len(possible_patterns) == 1:
+            return True
+
+    return False
+
+        
 if __name__ == '__main__':
-
     with open('input.txt', 'r') as f:
         for line in f:
             pid_ranges = line.strip('\n')
@@ -48,7 +87,8 @@ if __name__ == '__main__':
 
     for pid_range in pid_ranges.split(','):
         for pid in expand_pid_range(pid_range):
-            if is_pid_invalid(pid):
+            # if is_pid_invalid(pid):
+            if is_pid_invalid_v2(pid):
                 pid_sum += pid
 
     print(pid_sum)
